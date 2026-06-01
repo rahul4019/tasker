f6b4278be0d1f7f33709f032f204c3f9c8034e17
rahul4019 <rahulkashyap4019@gmail.com>
2026-06-01 06:46:18 +0530

    Add Todo model

--- apps/backend/internal/model/todo/todo.go
+++ apps/backend/internal/model/todo/todo.go
@@ -1,6 +1,11 @@
 package todo
 
-import "github.com/rahul4019/tasker/internal/model"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rahul4019/tasker/internal/model"
+)
 
 type Status string
 
@@ -19,6 +24,25 @@ const (
 	PriorityHigh   Priority = "high"
 )
 
+// * All the fields which can be NULL(nullable) are of type pointer because it zero value of reference type is nill
 type Todo struct {
 	model.Base
+	UserID       string     `json:"userId" db:"user_id"`
+	Title        string     `json:"title" db:"title"`
+	Description  *string    `json:"description" db:"description"`
+	Status       Status     `json:"status" db:"status"`
+	Priority     Priority   `json:"priority" db:"priority"`
+	DueDate      *time.Time `json:"DueDate" db:"due_date"`
+	CommpletedAt *time.Time `json:"commpletedAt" db:"commpleted_at"`
+	ParentTodoID *uuid.UUID `json:"parentTodoId" db:"parent_todo_id"`
+	CategoryID   *uuid.UUID `json:"categoryId" db:"category_id"`
+	Metadata     *Metadata  `json:"metadata" db:"metadata"`
+	SortOrder    int        `json:"sortOrder" db:"sort_order"`
+}
+
+type Metadata struct {
+	Tags       []string `json:"tags"`
+	Reminder   *string  `json:"reminder"`
+	Color      *string  `json:"color"`
+	Difficulty *int     `json:"difficulty"`
 }
