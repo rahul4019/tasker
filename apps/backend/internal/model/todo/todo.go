7212a4aa7db1e93fe3929eb85661a3e88a7102a3
rahul4019 <rahulkashyap4019@gmail.com>
2026-06-05 07:04:41 +0530

    Add PopulatedTodo struct and update GetTodoByID method to include category, children, and comments

--- apps/backend/internal/model/todo/todo.go
+++ apps/backend/internal/model/todo/todo.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/rahul4019/tasker/internal/model"
+	"github.com/rahul4019/tasker/internal/model/category"
+	"github.com/rahul4019/tasker/internal/model/comment"
 )
 
 type Status string
@@ -45,4 +47,11 @@ type Metadata struct {
 	Reminder   *string  `json:"reminder"`
 	Color      *string  `json:"color"`
 	Difficulty *int     `json:"difficulty"`
+}
+
+type PopulatedTodo struct {
+	Todo
+	Category *category.Category `json:"category" db:"category"`
+	Children []Todo             `json:"children" db:"children"`
+	Comments []comment.Comment  `json:"comments" db:"comments"`
 }
--- apps/backend/internal/repository/todo.go
+++ apps/backend/internal/repository/todo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/rahul4019/tasker/internal/model/todo"
 	"github.com/rahul4019/tasker/internal/server"
@@ -66,7 +67,61 @@ func (r *TodoRepository) CreateTodo(ctx context.Context, userID string, payload 
 
 	todoItem, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[todo.Todo])
 	if err != nil {
-		return nil, fmt.Errorf("failed to execute create todo query for user_id=%s title=%s: %w", userID, payload.Title, err)
+		return nil, fmt.Errorf("failed to collect row from table:todos for user_id=%s title=%s: %w", userID, payload.Title, err)
 	}
+	return &todoItem, nil
+}
+
+func (r *TodoRepository) GetTodoByID(ctx context.Context, userID string, todoID uuid.UUID) (*todo.PopulatedTodo, error) {
+	stmt := `
+		SELECT
+			t.*,
+			CASE
+				WHEN c.id IS NOT NULL THEN to_jsonb(camel (c))
+				ELSE NULL
+			END AS category,
+			COALESCE(
+				jsonb_agg(
+					to_jsonb(camel (child))
+					ORDER BY
+						child.sort_order ASC,
+						child.created_at ASC,
+				) FILTER (
+					WHERE 
+						child.id IS NOT NULL	 
+				),
+				'[]'::JSONB
+			) AS children
+		FROM
+			todos t
+			LEFT JOIN todo_categories c ON c.id=t.category_id
+			AND c.user_id=@user_id
+			LEFT JOIN todos child ON child.parent_todo_id=t.id
+			AND child.user_id=@user_id
+			LEFT JOIN todo_comments com ON com.todo_id=t.id
+			AND com.user_id=@user_id
+		WHERE
+			t.id=@id
+			AND t.user_id=@user_id
+		GROUP BY
+			t.id,
+			c.id
+
+`
+
+	rows, err := r.server.DB.Pool.Query(ctx, stmt, pgx.NamedArgs{
+		"id":      todoID,
+		"user_id": userID,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute get todo by id query for todo_id=%s user_id=%s: %w", todoID.String(), userID, err)
+	}
+
+	todoItem, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[todo.PopulatedTodo])
+	if err != nil {
+		return nil, fmt.Errorf("failed to collect row from table:todos for todo_id=%s user_id=%s: %w", todoID.String(), userID, err)
+	}
+
 	return &todoItem, nil
 }
