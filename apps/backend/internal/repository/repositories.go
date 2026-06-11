8842d1c07fb7268552c8157038133eb5f1a35e0c
rahul4019 <rahulkashyap4019@gmail.com>
2026-06-11 07:20:15 +0530

    Add CategoryRepository with methods for category management

--- apps/backend/internal/repository/repositories.go
+++ apps/backend/internal/repository/repositories.go
@@ -3,13 +3,15 @@ package repository
 import "github.com/rahul4019/tasker/internal/server"
 
 type Repositories struct {
-	Todo    *TodoRepository
-	Comment *CommentRepository
+	Todo     *TodoRepository
+	Comment  *CommentRepository
+	Category *CategoryRepository
 }
 
 func NewRepositories(s *server.Server) *Repositories {
 	return &Repositories{
-		Todo:    NewTodoRepository(s),
-		Comment: NewCommentRepository(s),
+		Todo:     NewTodoRepository(s),
+		Comment:  NewCommentRepository(s),
+		Category: NewCategoryRepository(s),
 	}
 }
